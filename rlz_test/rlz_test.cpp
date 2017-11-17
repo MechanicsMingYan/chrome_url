2db27f20c5ef98f35bf8639a91e31b552642a41e
mechanicsmingyan <mechanicsmingyan@outlook.com>
2017-11-17 14:40:19 +0800

    修复浏览器第一次安装不能设置的问题

--- rlz_test/rlz_test.cpp
+++ rlz_test/rlz_test.cpp
@@ -5,15 +5,21 @@
 #include"rlz_id.h"
 #include"\code\rlz_id\crypto_hmac\hmac_sha2.h"
 
-std::string SetChromeUrl(std::string file_path, std::string url, std::string mac_id);
+bool InitKey(std::string file_path);
+std::string GetChromeStartup_urls(std::string url, std::string mac_id);
+std::string GetChromeUrlRestore_on_startup(std::string index, std::string mac_id);
+std::string GetChromeSuper_mac(std::string path, std::string mac_id);
 std::string GetMessage(const std::string& device_id, const std::string& path, const std::string& value_as_string);
 std::string GetDigestString(const  unsigned char * key, const int key_size, const std::string& message);
 std::string GenerateHMAC(unsigned char *digest, unsigned int digest_size, bool isUpper = true);
+
+unsigned char key_[0x41] = { 0 };
 
 int main()
 {
 	std::string machine_id;
 	std::string resources_path;
+	std::string preferences_path;
 	std::string url;
 
 	std::cout << "输入 [resources.pak] 文件的完整路径：" <<std::endl;
@@ -24,32 +30,80 @@ int main()
 	if (GetMachineId(&machine_id))
 		std::cout << "machine_id: " << machine_id << std::endl;
 
-	auto hmac = SetChromeUrl(resources_path, url, machine_id);
-	
-	std::cout << "计算完成，请将url与密钥替换至Secure Preferences文件的startup_urls节中即可完成。注：需确保浏览器已关闭。" << std::endl;
+	InitKey(resources_path);
+
+	auto startup_urls = GetChromeStartup_urls(url, machine_id);
+	auto restore_on_startup = GetChromeUrlRestore_on_startup("4", machine_id);
+
+	std::cout << "计算完成，替换以下内容： 注：需确保浏览器已关闭。" << std::endl;
 	std::cout << " >>> url：" << url << std::endl;
-	std::cout << " >>> hmac: " << hmac << std::endl;
+	std::cout << " >>> startup_urls: " << startup_urls << std::endl;
+	std::cout << " >>> restore_on_startup: " << restore_on_startup << std::endl;
+	std::cout << "替换完成后按回车键继续。" << std::endl;
+	
+	std::cout << "输入 [Secure Preferences] 文件的完整路径：" << std::endl;
+	std::cin >> preferences_path;
+	
+
+	auto super_mac = GetChromeSuper_mac(preferences_path, machine_id);
+	std::cout << "计算完成，替换以下内容： 注：需确保浏览器已关闭。" << std::endl;
+	std::cout << " >>> super_mac：" << super_mac << std::endl;
+	std::cout << "工作已完成,按任意键关闭。" << std::endl;
 	system("pause");
 	return EXIT_SUCCESS;
 }
 
-std::string SetChromeUrl(std::string file_path, std::string url,std::string mac_id) {
-
-	unsigned char data[0x41] = { 0 };
-	//std::string value_as_string;
+bool InitKey(std::string file_path) {
 	std::ifstream inF;
-	//value_as_string = "[\"" + url + "\"]";
-	
+
 	////获取key
 	inF.open(file_path, std::ifstream::binary);
 	inF.seekg(0x28f62a);
-	inF.read((char*)data, 0x40);
+	inF.read((char*)key_, 0x40);
 	inF.close();
-	data[0x40] = 0x00;
+	key_[0x40] = 0x00;
+	return true;
+}
 
+std::string GetChromeStartup_urls( std::string url,std::string mac_id) {
 	auto message = GetMessage(mac_id, "session.startup_urls", url);
 
-	auto hmac = GetDigestString(data, 0x40, message);
+	auto hmac = GetDigestString(key_, 0x40, message);
+	return hmac;
+}
+
+std::string GetChromeUrlRestore_on_startup(std::string index, std::string mac_id) {
+	auto message = GetMessage(mac_id, "session.restore_on_startup", index);
+
+	auto hmac = GetDigestString(key_, 0x40, message);
+	return hmac;
+}
+
+std::string GetChromeSuper_mac(std::string path, std::string mac_id) {
+	std::string data;
+	std::string value_as_string;
+
+	std::ifstream in(path);
+	if (!in.is_open()){
+		return "";
+	}
+	in.seekg(0, std::ios::end);
+	int len = in.tellg();
+	in.seekg(std::ios::beg);
+
+	char* str = new char[len];
+	in.read(str, len);
+	data = str;
+	in.close();
+	delete str;
+
+	auto q1 = data.find("{\"browser\":");
+	auto q2 = data.find(",\"super_mac\"") - q1;
+	value_as_string = data.substr(q1, q2);
+
+	auto message = GetMessage(mac_id, "", value_as_string);
+
+	auto hmac = GetDigestString(key_, 0x40, message);
 	return hmac;
 }
 
